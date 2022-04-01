a933c64b85f2e1fb6c8b7bf242445ca634731e42
Isaicsefxec <64870459+Isaicsefxec@users.noreply.github.com>
2022-04-01 14:45:53 +0530

    Add files via upload

---
+++ Isai.js
@@ -0,0 +1,10 @@
+// import body from "../body/Body"
+ function Isai(props) {
+    return (
+      <div>
+          <img src={props.url} alt={props.altkey} />
+          <p>{props.name}</p>
+      </div>
+    );
+  }
+export default Isai;
